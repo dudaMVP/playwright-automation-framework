65b77bf79b487bd3cf4e92dec20efc1e7cca7bba
dudaMVP <hishmoe@gmail.com>
2025-08-02 19:11:57 -0500

    checkpoint before integrating azure pipelines

--- tests/login/member.logins.spec.ts
+++ tests/login/member.logins.spec.ts
@@ -1,1 +1,46 @@
 import { test, expect } from './../../fixtures/PageFixture'
+
+test.describe('Member Login Test Cases @Regression', () => {
+    test('')
+})
+//     test('Login with invalid username and correct password → error message appears @Smoke', async ({ loginPage }) => {
+//         await loginPage.navigateToMemberLogin()
+//         aw:qait expect(loginPage.memberLoginSign).toBeVisible()
+//         await loginPage.memberLogin('invalidUsername', process.env.patientPassword!)
+//         await expect(loginPage.invalidCredentialsError).toBeVisible()
+//         await expect(loginPage.invalidCredentialsError).toHaveText('Login FailedInvalid credentials. Please try again.')
+//     })
+
+//     test('Login with invalid username and invalid password → error message appears @Smoke', async ({ loginPage }) => {
+//         await loginPage.navigateToMemberLogin()
+//         await expect(loginPage.memberLoginSign).toBeVisible()
+//         await loginPage.memberLogin('invalidUsername', 'invalidPassword')
+//         await expect(loginPage.invalidCredentialsError).toBeVisible()
+//         await expect(loginPage.invalidCredentialsError).toHaveText('Login FailedInvalid credentials. Please try again.')
+//     })
+//     test('Login with empty username and empty password → error message appears @Smoke', async ({ loginPage }) => {
+//         await loginPage.navigateToMemberLogin()
+//         await expect(loginPage.memberLoginSign).toBeVisible()
+//         await loginPage.memberLogin('', '')
+//         await expect(loginPage.emptyUsernameError).toBeVisible()
+//         await expect(loginPage.emptyPasswordError).toBeVisible()
+//         await expect(loginPage.emptyUsernameError).toHaveText('Username is required')
+//         await expect(loginPage.emptyPasswordError).toHaveText('Password is required')
+//     })
+//     test('Login with empty username and correct password → error message appears @Smoke', async ({ loginPage }) => {
+//         await loginPage.navigateToMemberLogin()
+//         await expect(loginPage.memberLoginSign).toBeVisible()
+//         await loginPage.memberLogin('', process.env.patientPassword!)
+//         await expect(loginPage.emptyUsernameError).toBeVisible()
+//         await expect(loginPage.emptyPasswordError).not.toBeVisible()
+//         await expect(loginPage.emptyUsernameError).toHaveText('Username is required')
+//     })
+//     test.only('Login with correct username and empty password → error message appears @Smoke', async ({ loginPage }) => {
+//         await loginPage.navigateToMemberLogin()
+//         await expect(loginPage.memberLoginSign).toBeVisible()
+//         await loginPage.memberLogin(process.env.patientUsername!, '')
+//         await expect(loginPage.emptyUsernameError).not.toBeVisible()
+//         await expect(loginPage.emptyPasswordError).toBeVisible()
+//         await expect(loginPage.emptyPasswordError).toHaveText('Password is required')
+//     })
+
--- tests/smoke/login.smoke.spec.ts
+++ tests/smoke/login.smoke.spec.ts
@@ -16,51 +16,10 @@ test.describe('Smoke Suite - Critical Functions Only', () => {
     })
 
     test('Validate Successful Member login @Smoke @Login', async ({ memberLoginPage }) => {
-        console.log(memberLoginPage.url())
+        console.log(memberLoginPage.page.url())
         await memberLoginPage.memberLogin()
         await expect(memberLoginPage.page).toHaveURL(/\/patient\/dashboard/)
     })
 
     
-
-//     test('Login with invalid username and correct password → error message appears @Smoke', async ({ loginPage }) => {
-//         await loginPage.navigateToMemberLogin()
-//         aw:qait expect(loginPage.memberLoginSign).toBeVisible()
-//         await loginPage.memberLogin('invalidUsername', process.env.patientPassword!)
-//         await expect(loginPage.invalidCredentialsError).toBeVisible()
-//         await expect(loginPage.invalidCredentialsError).toHaveText('Login FailedInvalid credentials. Please try again.')
-//     })
-
-//     test('Login with invalid username and invalid password → error message appears @Smoke', async ({ loginPage }) => {
-//         await loginPage.navigateToMemberLogin()
-//         await expect(loginPage.memberLoginSign).toBeVisible()
-//         await loginPage.memberLogin('invalidUsername', 'invalidPassword')
-//         await expect(loginPage.invalidCredentialsError).toBeVisible()
-//         await expect(loginPage.invalidCredentialsError).toHaveText('Login FailedInvalid credentials. Please try again.')
-//     })
-//     test('Login with empty username and empty password → error message appears @Smoke', async ({ loginPage }) => {
-//         await loginPage.navigateToMemberLogin()
-//         await expect(loginPage.memberLoginSign).toBeVisible()
-//         await loginPage.memberLogin('', '')
-//         await expect(loginPage.emptyUsernameError).toBeVisible()
-//         await expect(loginPage.emptyPasswordError).toBeVisible()
-//         await expect(loginPage.emptyUsernameError).toHaveText('Username is required')
-//         await expect(loginPage.emptyPasswordError).toHaveText('Password is required')
-//     })
-//     test('Login with empty username and correct password → error message appears @Smoke', async ({ loginPage }) => {
-//         await loginPage.navigateToMemberLogin()
-//         await expect(loginPage.memberLoginSign).toBeVisible()
-//         await loginPage.memberLogin('', process.env.patientPassword!)
-//         await expect(loginPage.emptyUsernameError).toBeVisible()
-//         await expect(loginPage.emptyPasswordError).not.toBeVisible()
-//         await expect(loginPage.emptyUsernameError).toHaveText('Username is required')
-//     })
-//     test.only('Login with correct username and empty password → error message appears @Smoke', async ({ loginPage }) => {
-//         await loginPage.navigateToMemberLogin()
-//         await expect(loginPage.memberLoginSign).toBeVisible()
-//         await loginPage.memberLogin(process.env.patientUsername!, '')
-//         await expect(loginPage.emptyUsernameError).not.toBeVisible()
-//         await expect(loginPage.emptyPasswordError).toBeVisible()
-//         await expect(loginPage.emptyPasswordError).toHaveText('Password is required')
-//     })
  })
